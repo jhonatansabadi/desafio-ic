9f293dc851fe0c989e5577b97183c726ebc092a2
jhonatansabadi <jhonatansabadi@hotmail.com>
2020-04-22 19:13:43 -0300

    Criação de modelo de dados

---
+++ app/src/main/java/jhonatan/sabadi/inchurch/model/Result.kt
@@ -0,0 +1,15 @@
+package jhonatan.sabadi.inchurch.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Result(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("total_results")
+    val totalResults: Int,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("results")
+    val results: List<Movie>
+)
