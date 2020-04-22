3952ed25d6bdc17bbd83811275f83ade11371b1a
jhonatansabadi <jhonatansabadi@hotmail.com>
2020-04-22 20:26:23 -0300

    Implementando ViewModel com Repository

--- app/src/main/java/jhonatan/sabadi/inchurch/repository/MovieRepository.kt
+++ app/src/main/java/jhonatan/sabadi/inchurch/repository/MovieRepository.kt
@@ -1,2 +1,21 @@
 package jhonatan.sabadi.inchurch.repository
 
+import android.content.Context
+import jhonatan.sabadi.inchurch.api.call.MovieApi
+import jhonatan.sabadi.inchurch.api.retrofit.RetrofitService
+import jhonatan.sabadi.inchurch.model.Movie
+import retrofit2.await
+
+class MovieRepository(
+    private val context: Context
+) {
+
+    private val retrofit by lazy {
+        RetrofitService.createService(MovieApi::class.java)
+    }
+
+    suspend fun getMovies(): List<Movie> {
+        val data = retrofit.getMovies(page = 1).await()
+        return data
+    }
+}
--- app/src/main/java/jhonatan/sabadi/inchurch/ui/viewmodel/MovieViewModel.kt
+++ app/src/main/java/jhonatan/sabadi/inchurch/ui/viewmodel/MovieViewModel.kt
@@ -1,2 +1,33 @@
 package jhonatan.sabadi.inchurch.ui.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.liveData
+import jhonatan.sabadi.inchurch.model.Movie
+import jhonatan.sabadi.inchurch.repository.MovieRepository
+import jhonatan.sabadi.inchurch.ui.viewmodel.resource.Resource
+import java.lang.Exception
+
+class MovieViewModel(
+    application: Application
+) : AndroidViewModel(application) {
+
+    private val movieRepository by lazy { MovieRepository(application) }
+
+    val movies: LiveData<Resource<List<Movie>>>
+        get() = _movies
+
+    private val _movies = liveData {
+        emit(Resource.Loading())
+        try {
+            val data = movieRepository.getMovies()
+            emit(Resource.Success(data))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(Resource.Failure())
+        }
+    }
+
+}
